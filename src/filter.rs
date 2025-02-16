7c8d22ff79c723af28ed13843ae1c34d33e1687f
viniguim4 <viniguima14@gmail.com>
2025-02-16 13:12:37 -0300

    [FEAT] Raydium sizes.

--- src/filter.rs
+++ src/filter.rs
@@ -6,11 +6,25 @@ const WSOL : &'static str = "So11111111111111111111111111111111111111112";
 
 #[derive(Debug)]
 pub enum RaydiumType {
-    Buy,
-    Sell,
-    AddLiquidity,
-    RemoveLiquidity,
+    Buy(TradeSize),
+    Sell(TradeSize),
+    AddLiquidity(LiquiditySize),
+    RemoveLiquidity(LiquiditySize),
     Unknown
+}
+
+#[derive(Debug)]
+pub struct TradeSize {
+    reserve_in: u64,
+    reserve_out: u64,
+    amount_in: u64,
+    amount_out : u64,
+    price_impact: f64
+}
+#[derive(Debug)]
+pub struct LiquiditySize {
+    sol_reserve : u64,
+    token_reserve : u64
 }
 
 impl RaydiumType {
@@ -83,15 +97,24 @@ impl RaydiumType {
            post_token_balances == 0 {
             return Err(anyhow::anyhow!("Failed to indentify transaction type"));
         }
-
+        let sol_reserve = post_sol_balances - pre_sol_balances;
+        let token_reserve = post_token_balances - pre_token_balances;
         if (post_sol_balances > pre_sol_balances) && (post_token_balances > pre_token_balances) {
-            return Ok(RaydiumType::AddLiquidity);
+            return Ok(RaydiumType::AddLiquidity(LiquiditySize{sol_reserve, token_reserve}));
         } else if (post_sol_balances < pre_sol_balances) && (post_token_balances < pre_token_balances) {
-            return Ok(RaydiumType::RemoveLiquidity);
+            return Ok(RaydiumType::RemoveLiquidity(LiquiditySize{sol_reserve, token_reserve}));
         } else if (post_sol_balances > pre_sol_balances) && (post_token_balances < pre_token_balances) {
-            return Ok(RaydiumType::Buy);
+            let price_impact_sqrt = post_sol_balances as f64/pre_sol_balances as f64;
+            let price_impact = (price_impact_sqrt * price_impact_sqrt) - 1.0; 
+            let amount_in = post_sol_balances - pre_sol_balances;
+            let amount_out = pre_token_balances - post_token_balances;
+            return Ok(RaydiumType::Buy(TradeSize{reserve_in : post_sol_balances, reserve_out :post_token_balances, amount_in, amount_out, price_impact}));
         } else if (post_sol_balances < pre_sol_balances) && (post_token_balances > pre_token_balances) {
-            return Ok(RaydiumType::Sell);
+            let price_impact_sqrt = pre_token_balances as f64/ post_token_balances as f64;
+            let price_impact = 1.0 - (price_impact_sqrt * price_impact_sqrt);
+            let amount_in = post_token_balances - pre_token_balances;
+            let amount_out = pre_sol_balances - post_sol_balances;
+            return Ok(RaydiumType::Sell(TradeSize{reserve_in : post_token_balances, reserve_out : post_sol_balances, amount_in, amount_out, price_impact}));
         } else {
             return Ok(RaydiumType::Unknown);
         }
